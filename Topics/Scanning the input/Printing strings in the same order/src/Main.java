ceff2586faea2ac57767563a037765015b165f96
Rinze-CodeGorilla <132885446+Rinze-CodeGorilla@users.noreply.github.com>
2023-05-30 12:59:15 +0200

    Initial commit

---
+++ Topics/Scanning the input/Printing strings in the same order/src/Main.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        var one = scanner.next();
+        var two = scanner.next();
+        var three = scanner.next();
+        var four = scanner.next();
+
+        System.out.println(one);
+        System.out.println(two);
+        System.out.println(three);
+        System.out.println(four);
+    }
+}
